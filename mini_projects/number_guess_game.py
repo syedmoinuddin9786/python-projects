d0a15f07d993491233e887c26ab2f53488f75243
Syed Moinuddin <syedmoinuddin3201@gmail.com>
2026-02-13 16:50:14 +0530

    Update number_guess_game.py
    Improved Number Guessing Game with attempt limit

--- mini_projects/number_guess_game.py
+++ mini_projects/number_guess_game.py
@@ -2,11 +2,13 @@ import random
 
 number = random.randint(1, 10)
 attempts = 0
+max_attempts = 5
 
 print("Welcome to the Number Guessing Game!")
 print("Guess a number between 1 and 10")
+print("You have", max_attempts, "attempts")
 
-while True:
+while attempts < max_attempts:
     guess = int(input("Enter your guess: "))
     attempts += 1
 
@@ -17,3 +19,6 @@ while True:
     else:
         print("Correct! You guessed it in", attempts, "attempts.")
         break
+
+if attempts == max_attempts and guess != number:
+    print("Game Over! The correct number was:", number)
